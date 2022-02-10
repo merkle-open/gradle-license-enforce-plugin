45c8f60b72e06fec1a1cdbea3395ec0a8027b80c
aschaefer <andre.schaefer@mgb.ch>
2022-02-10 16:21:12 +0100

    fix(dependencies): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,23 +5,23 @@ plugins {
     kotlin("jvm") version embeddedKotlinVersion
     `kotlin-dsl`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.17.0"
+    id("com.gradle.plugin-publish") version "0.20.0"
     `java-gradle-plugin`
     id("fr.brouillard.oss.gradle.jgitver") version "0.10.0-rc03"
     id("com.github.hierynomus.license-base") version "0.16.1"
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 dependencies {
     implementation(platform(kotlin("bom")))
 
-    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("org.dom4j:dom4j:2.1.3")
-    implementation("org.yaml:snakeyaml:1.29")
-    implementation("xerces:xercesImpl:2.12.1")
+    implementation("org.yaml:snakeyaml:1.30")
+    implementation("xerces:xercesImpl:2.12.2")
 
     testImplementation(kotlin("test"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 repositories {
